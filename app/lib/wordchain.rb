1ae7c792dc037facf09d26eb9a0b8713ca1e579b
Dean <deangalvin3@gmail.com>
2016-04-28 15:40:57 -0400

    choose correct next word

--- app/lib/wordchain.rb
+++ app/lib/wordchain.rb
@@ -3,22 +3,22 @@ require_relative '../models/entries'
 
 class WordChain
   def self.next_word(previous_tokens)
-    tokens = Entries.where(word: previous_tokens[0], nword: previous_tokens[1]).order(count: :desc)
+    tokens = Entries.where(word: previous_tokens[0], nword: previous_tokens[1]).order(count: :desc).pluck(:definition)
     find_best_token(tokens)
   end
 
   def self.bigram_word(previous_token)
     tokens = Entries.where(word: previous_token).order(count: :desc)
-    find_best_token(tokens)
+    find_best_token(tokens.map(&:nword))
   end
 
   private
   def self.find_best_token(tokens)
     return "" if(tokens.length == 0)
 
-    tokens.map(&:definition).each do |token|
+    tokens.each do |token|
       return token if rand < 0.9
     end
-    return tokens.last.definition
+    return token.last
   end
 end
