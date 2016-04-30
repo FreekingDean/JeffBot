206e1a5f3c8a7a4b911f632d9a592a7c96193d11
Dean <deangalvin3@gmail.com>
2016-04-30 17:20:51 -0400

    use true popular choice

--- app/lib/wordchain.rb
+++ app/lib/wordchain.rb
@@ -8,26 +8,26 @@ class WordChain
     def next_word(previous_tokens)
       find_best_token(
         Entries.where(word: previous_tokens[0], nword: previous_tokens[1])
-          .order(count: :desc).pluck(:definition)
+          .order(count: :desc).pluck(:definition, :count).to_h
       )
     end
 
     def bigram_word(previous_token)
       find_best_token(
-        Entries.where(word: previous_token).order(count: :desc).pluck(:nword)
+        Entries.where(word: previous_token).order(count: :desc).pluck(:nword, :count).to_h
       )
     end
 
     private
 
     def find_best_token(tokens)
-      return '' if tokens.length.blank?
+      return '' if tokens.any?
 
-      tokens.each do |token|
-        return token if rand < 0.75
+      tokens.each do |word, count|
+        return word if rand < (count/(tokens.values.inject(:+)))
       end
 
-      tokens.last
+      tokens.keys.last
     end
   end
 end
