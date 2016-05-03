ed47568c8e45d1ecdfca40136b820751488f03ce
Dean <deangalvin3@gmail.com>
2016-05-02 23:14:04 -0400

    fixed issues with loader

--- config/application.rb
+++ config/application.rb
@@ -1,16 +1,20 @@
-require "active_record"
-require "active_support"
-require "active_support/core_ext"
-require "bunny"
+require 'active_record'
+require 'active_support'
+require 'active_support/core_ext'
+require 'bunny'
 
 # require config files
-Dir["#{File.dirname(__FILE__)}/../config/*.rb"].each { |file| require file }
+Dir["#{File.dirname(__FILE__)}/../config/*.rb"].each do |file|
+  require file
+end
 
 # require initializers
-Dir["#{File.dirname(__FILE__)}/../config/initializers/*.rb"].each { |file| require file }
+Dir["#{File.dirname(__FILE__)}/../config/initializers/*.rb"].each do |file|
+  require file
+end
 
 # require app files
-Dir["#{File.dirname(__FILE__)}/../app/**/*.rb"].each { |file|
-  puts file
+Dir["#{File.dirname(__FILE__)}/../app/**/*.rb"].each do |file|
+  next if file.contains?('app/main.rb')
   require file
-}
+end
