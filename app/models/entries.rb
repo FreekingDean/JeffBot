446f38eb5f3e924570a48b0f3fa9b09b877c3706
Dean <deangalvin3@gmail.com>
2016-12-12 12:41:02 -0500

    removed logging

--- app/models/entries.rb
+++ app/models/entries.rb
@@ -1,9 +1,7 @@
 class Entries < ActiveRecord::Base
   def self.get_words(gram_1:, gram_2: nil)
-    ap gram_2
     scope = Entries.select('word, sum(count) as total').where(gram_1: gram_1)
     scope = scope.where(gram_2: gram_2) unless gram_2.nil?
-    ap scope.to_sql
     scope.group(:word).order('total DESC').to_a
   end
 end
