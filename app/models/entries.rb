1ca6cc1d59aed777f4be791264010daf5f3490e1
Dean <deangalvin3@gmail.com>
2016-12-28 10:38:00 -0500

    Re write count

--- app/models/entries.rb
+++ app/models/entries.rb
@@ -1,9 +1,7 @@
 class Entries < ActiveRecord::Base
-  attr_accessor :total
-
   def self.get_words(gram_1:, gram_2: nil)
-    scope = Entries.select('word, sum(count) as total').where(gram_1: gram_1)
+    scope = Entries.select('word, sum(count) as count').where(gram_1: gram_1)
     scope = scope.where(gram_2: gram_2) unless gram_2.nil?
-    scope.group(:word).order('total DESC').to_a
+    scope.group(:word).order('count DESC').to_a
   end
 end
