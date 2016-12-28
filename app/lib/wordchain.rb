bbb2e1cbe817c6286469e4da0030cf57f5312cc6
Dean <deangalvin3@gmail.com>
2016-12-28 10:05:29 -0500

    Change randomization

--- app/lib/wordchain.rb
+++ app/lib/wordchain.rb
@@ -22,7 +22,7 @@ class WordChain
       total_count = tokens.map(&:total).inject(0, :+)
       tokens.each do |entries|
         old_count += entries.total
-        return entries.word if (rand * 0.6) < (old_count / total_count)
+        return entries.word if (rand * 1.25) < (old_count / total_count)
       end
 
       tokens.last.word
