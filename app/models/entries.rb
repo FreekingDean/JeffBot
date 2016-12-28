d16ebb2f65e5fde117c14b2a9060ada1822461f7
Dean <deangalvin3@gmail.com>
2016-12-28 10:36:53 -0500

    Testing a total

--- app/models/entries.rb
+++ app/models/entries.rb
@@ -1,4 +1,6 @@
 class Entries < ActiveRecord::Base
+  attr_accessor :total
+
   def self.get_words(gram_1:, gram_2: nil)
     scope = Entries.select('word, sum(count) as total').where(gram_1: gram_1)
     scope = scope.where(gram_2: gram_2) unless gram_2.nil?
